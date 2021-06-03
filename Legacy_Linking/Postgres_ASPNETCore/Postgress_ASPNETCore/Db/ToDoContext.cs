0113a6f51dd7ecb4f520bbcf6bec1af9d1411e45
Adrita Sharma <adritasharma.developer@gmail.com>
2021-06-03 16:22:53 +0530

    folder structure modified

---
+++ Legacy_Linking/Postgres_ASPNETCore/Postgress_ASPNETCore/Db/ToDoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Postgress_ASPNETCore.Db.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Postgress_ASPNETCore.Db
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
---
+++ Legacy_Linking/Postgres_ASPNETCore/Postgress_ASPNETCore/Reposirories/IToDoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Postgress_ASPNETCore.Db.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Postgress_ASPNETCore.Reposirories
+{
+    public interface IToDoRepository
+    {
+        public IEnumerable<ToDo> GetAllToDos();
+    }
+}
