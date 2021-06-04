5d78e83eb5e130affa37a77419d517835597258d
Adrita Sharma <adritasharma.developer@gmail.com>
2021-06-04 17:14:56 +0530

    deployment working

---
+++ Kubernetes/NodejsApp/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Kubernetes');
+});
+
+
+app.listen(PORT, HOST);
+
+console.log(`Running version 3 on http://${HOST}:${PORT}`);
